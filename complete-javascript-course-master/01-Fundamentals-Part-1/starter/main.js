c069776bbc55ecf0c6ef484002b520f0ec2f0be3
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-12-16 15:37:48 +0530

    pre-formate

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,23 @@
+let js = "amezing";
+
+console.log(40 + 47 + 12 - 45);
+
+console.log("maulik");
+console.log("akshay");
+console.log("45");
+console.log(45);
+console.log("45mow");
+
+let firstName = "Maulik";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "program";
+let myCurrentJob = "web developer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
