2289e1c0a183941b404fa9b7fa237765f956d845
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-12-16 16:14:28 +0530

    first

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+// LECTURE: Values and Variables
+
+let country = "India";
+
+let continent = "Asia";
+
+let population = "1.2 billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/main.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/main.js
@@ -1,7 +1,11 @@
+/*
+------------Values and Variables-------------
+
 let js = "amezing";
 
 console.log(40 + 47 + 12 - 45);
 
+// Variable name conventions
 console.log("maulik");
 console.log("akshay");
 console.log("45");
@@ -21,3 +25,20 @@ let myCurrentJob = "web developer";
 
 console.log(myFirstJob);
 console.log(myCurrentJob);
+
+*/
+
+/*
+------ Data Type-----------
+*/
+
+console.log(true);
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof true);
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof 23.5454);
+console.log(typeof "Maulik");
