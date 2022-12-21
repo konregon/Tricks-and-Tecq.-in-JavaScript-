c5780995a3932c608515ac124543fc3f280324c2
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2022-12-21 17:47:15 +0530

    challenge 1

---
+++ Challanges/challange 1/main.js
@@ -0,0 +1,29 @@
+// test data 1
+let markMass1 = 78;
+let johnMass1 = 92;
+
+const markHeight1 = 1.69;
+const johnHeight1 = 1.92;
+
+let markBmi1 = markMass1 / markHeight1 ** 2;
+console.log(markBmi1);
+let johnBmi1 = johnMass1 / johnHeight1 ** 2;
+console.log(johnBmi1);
+
+let markHigherBMI1 = markBmi1 > johnBmi1;
+console.log(markHigherBMI1);
+
+// test data 2
+let markMass2 = 95;
+let johnMass2 = 85;
+
+const markHeight2 = 1.88;
+const johnHeight2 = 1.76;
+
+let markBmi2 = markMass2 / markHeight2 ** 2;
+console.log(markBmi2);
+let johnBmi2 = johnMass2 / johnHeight2 ** 2;
+console.log(johnBmi2);
+
+let markHigherBMI2 = markBmi2 > johnBmi2;
+console.log(markHigherBMI2);
