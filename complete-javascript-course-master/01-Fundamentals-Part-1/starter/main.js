d14c6f43d0a9075f8d85af710bc47f63ba69267d
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2022-12-27 17:03:03 +0530

    The Conditional (Ternary) Operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/main.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/main.js
@@ -315,3 +315,25 @@ switch (day) {
 /*
 -------- The Conditional (Ternary) Operator --------
 */
+
+const age = 26;
+age >= 18
+  ? console.log("I like to drink wine!")
+  : console.log("I like to drink water");
+
+age >= 18 ? console.log("wine") : console.log("water");
+// turing this expression into value
+age >= 18 ? "wine" : "water"; // expression
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+// if go with if-else coding structure then
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
