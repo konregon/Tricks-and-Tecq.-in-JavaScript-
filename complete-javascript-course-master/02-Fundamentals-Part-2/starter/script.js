e2283c9496c651ffbdaf414738fa1b82c4a32e81
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2022-12-29 16:52:01 +0530

    Function Declarations vs. Expressions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -2,7 +2,7 @@
 
 /*
 ------ Functions ------
-*/
+
 function logger() {
   console.log("my name is Maulik Patel");
 }
@@ -25,3 +25,23 @@ console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(3, 3);
 console.log(appleOrangeJuice);
+*/
+/*
+------- Function Declaration vs. Expressions -----
+*/
+//Functions are just a VALUE
+//function declaration
+function calAge1(birthYear) {
+  // const age = 2023 - birthYear;
+  // return age; // or simply use return
+  return 2023 - birthYear;
+}
+const age1 = calAge1(1996);
+console.log(age1);
+
+//function expression
+const calAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calAge2(1998);
+console.log(age2);
