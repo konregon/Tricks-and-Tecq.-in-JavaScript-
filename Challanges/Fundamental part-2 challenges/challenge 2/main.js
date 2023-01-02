3a0db1950268594523ebe27bb8a8a2a08b1b7073
MAULIK RAJENDRA PATEL <60727880+konregon@users.noreply.github.com>
2023-01-02 18:50:25 +0530

    challenge 2 tips

---
+++ Challanges/Fundamental part-2 challenges/challenge 2/main.js
@@ -0,0 +1,21 @@
+// let bill = 1000;
+// // let tip = 15 < bill < 300 ? bill * 0.15 : bill * 0.2; // Wrong, we have to use && operator
+// let tip = bill <= 300 && bill >= 15 ? bill * 0.15 : bill * 0.2;
+
+// let total = tip + bill;
+
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}
+// `);
+
+function calcTip(bill) {
+  return bill <= 300 && bill >= 15 ? bill * 0.15 : bill * 0.2;
+}
+console.log(calcTip(2000));
+
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
